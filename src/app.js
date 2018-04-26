1670de03fc8f331d1920664c1f0c3476818890d4
Idrees <dargahidrees@gmail.com>
2018-04-26 09:25:55 +0530

    add files

---
+++ src/app.js
@@ -0,0 +1,145 @@
+import "./stylesheets/main.css";
+const request = require('request');
+const progress = require('request-progress');
+const cheerio = require('cheerio');
+const path = require('path');
+const fs = require('fs');
+const isOnline = require('is-online');
+const {
+  dialog,
+  app
+} = require('electron').remote;
+
+const helpers = require('./helpers/helpers');
+
+/* Download all the videos at once. */
+function downloadAllVideos(videos, name, dwnpath) {
+  let temp = helpers.findVidExist(videos, name, dwnpath);
+
+  /* the loopDownload function reiterates over the array and download the files one by one instead of downloading together. 
+  downloading together will reduce the speed and can cause crashes. */
+  const loopDownload = (video, name) => {
+    helpers.download(video[temp], name[temp], dwnpath, () => {
+      if (temp < video.length && name.length) {
+        temp++;
+        loopDownload(video, name);
+      }
+    });
+  }
+  loopDownload(videos, name);
+}
+
+/* Scrape the video download hrefs and course names from courseURL provided. */
+function getCourseNamesAndURLS(courseURL) {
+  return new Promise((resolve, reject) => {
+    request(courseURL, (err, body) => {
+      let chapterUrls = [];
+      let names = [];
+      if (!err) {
+        let $ = cheerio.load(body.body);
+        /* Filter out names of chapters and their urls from the current course url */
+        $('#lessons-list').filter(() => {
+          let data = $('#lessons-list');
+          let dataArray = data
+            .children('.lessons-list__li')
+            .children()
+            .toArray();
+
+          const filterChapterUrls = dataArray.filter(el => el.name === 'link' && el.attribs.itemprop === 'contentUrl');
+          const filterNames = dataArray.filter(el => el.name === 'span');
+
+          /* Here chapter names are fetched from the <span> tags */
+          filterNames.map(el => {
+            if (el.name === 'span') {
+              const videoName = el.children[0].data.replace(/[\/:*?"<>|]/g, '');
+              const name = helpers.replaceAll(videoName, 'Урок', 'Lesson')
+              names.push(name);
+            }
+          });
+
+          /* Here chapter urls are fetched from the <span> tags */
+          filterChapterUrls.map(el => {
+            chapterUrls.push(el.attribs.href);
+          });
+          resolve({
+            chapterUrls,
+            names
+          });
+        });
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
+
+/*  All Clientside Javascript */
+window.onload = () => {
+  isOnline().then(online => {
+    if (!online) {
+      alert("You are offline. Make sure you are connected to the internet. Exiting...");
+      app.exit(0);
+    }
+  });
+
+  let videoBlock,
+    vidName,
+    dwnBtn,
+    li,
+    resultul = helpers.getQuerySelector("#resultUL"),
+    getVideos = helpers.getQuerySelector("#getVids");
+
+  helpers.getQuerySelector(".progress").style.display = 'none';
+  getVideos.addEventListener("click", () => {
+    helpers.getQuerySelector("#overlay").style.display = 'block';
+    const url = helpers.getQuerySelector("#url").value;
+    let course_url_title = url.split('/');
+    getCourseNamesAndURLS(url)
+      .then(result => {
+        if(result) {
+          setTimeout(() => {
+            helpers.getQuerySelector("#overlay").style.display = 'none';
+          }, 3000)
+        }
+        helpers.getQuerySelector("#resultDiv").style.display = "block";
+        dwnBtn = document.createElement("button");
+        dwnBtn.id = "download";
+        dwnBtn.textContent = "Download All";
+        dwnBtn.classList.add("btn", "btn-dark");
+        helpers.getQuerySelector("#downloadWrap").appendChild(dwnBtn);
+
+        while (helpers.getQuerySelector('#resultUL').firstChild) {
+          helpers.getQuerySelector('#resultUL').removeChild(helpers.getQuerySelector('#resultUL').firstChild);
+        }
+
+        if (!helpers.getQuerySelector(".vid")) {
+          for (let x = 0; x < result.names.length; x++) {
+            li = document.createElement("li");
+            li.classList.add("list-group-item", "vid")
+            videoBlock = document.createElement("div");
+            videoBlock.className = 'vidNameBlock';
+            vidName = document.createElement("h6");
+            vidName.className = "chapter-name";
+            vidName.appendChild(document.createTextNode(`${result.names[x]}`));
+            videoBlock.appendChild(vidName);
+            //console.log(videoBlock);
+            li.appendChild(videoBlock);
+            resultul.appendChild(li);
+          }
+          dwnBtn.addEventListener("click", () => {
+            let dwnpath = dialog.showOpenDialog({
+              properties: ['openDirectory']
+            });
+
+            if(dwnpath === undefined) return;
+
+            if(!fs.existsSync(`${dwnpath}${path.sep}${course_url_title[course_url_title.length -1]}`)) {
+              fs.mkdirSync(`${dwnpath}${path.sep}${course_url_title[course_url_title.length -1]}`);
+            }
+            //console.log(`${dwnpath}${path.sep}${course_url_title[course_url_title.length -1]}`);
+            downloadAllVideos(result.chapterUrls, result.names, `${dwnpath}${path.sep}${course_url_title[course_url_title.length -1]}`);
+          });
+        }
+      });
+  });
+}
---
+++ src/helpers/helpers.js
@@ -0,0 +1,109 @@
+const fs = require('fs');
+const path = require('path');
+const request = require('request');
+const progress = require('request-progress');
+const {app} = require('electron').remote;
+
+const replaceAll = (target, search, replacement) => {
+  return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+const getQuerySelector = (selector) => {
+  return document.querySelector(selector);
+};
+
+const downloadTimeRemaining = (string, pad, length) => {
+    return (new Array(length+1).join(pad)+string).slice(-length);
+}
+
+const formatBytes = (bytes, decimals) => {
+    if(bytes == 0) return '0 Bytes';
+    var k = 1024,
+        dm = decimals || 2,
+        sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+        i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+ }
+
+const getDownloadedVideos = (downloadFolder) => {
+  const log = `${downloadFolder}${path.sep}chapters.txt`;
+  if (!fs.existsSync(log)) return [];
+  return fs.readFileSync(log).toString().split("\n");
+};
+
+/* Check if the video was downloaded completely. We insert the name of video into 'chapters.txt' after the download request is ended. */
+const isComplete = (videoName, dfolder) => {
+  const downloadedVideos = getDownloadedVideos(dfolder);
+  //console.log(downloadedVideos);
+  if (typeof downloadedVideos === 'undefined' || downloadedVideos.length === 0) {
+    return true;
+  }
+  for (let j = 0; j < downloadedVideos.length; j++) {
+    if (videoName === downloadedVideos[j])
+      return true;
+  }
+  return false;
+};
+/* Check if the video already exists in the directory. This is to avoid downloading files again. */
+const findVidExist = (videos, name, dwnpath) => {
+  let i = 0;
+  for (let x = 0; x < videos.length; x++) {
+    let filename = `${dwnpath}${path.sep}${name[x]}.mp4`
+    //console.log(filename);
+    if (fs.existsSync(filename) && isComplete(name[x], dwnpath)) {
+      console.log(`File \'${name[x]}\' already exists\n`);
+      i++;
+    } else {
+      break;
+    }
+  }
+  return i;
+};
+
+/* The basic function which downloads a video from the url */
+const downloadOne = (url, chapterName, dwnpath, nextVideo) => {
+  let req = progress(request(url), {
+      throttle: 2000,
+      delay: 1000
+    })
+    .on('progress', state => {
+      console.log(`Downloading: ${Math.floor(state.percent * 100)}% | ${Math.floor(state.speed / 1024) } kB/s | ${Math.floor(state.time.remaining)}s | ${Math.floor(state.size.transferred / 1024)} kilobytes`)
+
+      let remainingTime = state.time.remaining;
+      let minutes = Math.floor(remainingTime / 60);
+      let seconds = remainingTime - minutes * 60;
+      
+      getQuerySelector("#download").style.display = 'none';
+      getQuerySelector("#downloadWrap").style.display = 'block';
+      getQuerySelector(".progress").style.display = 'block';
+      getQuerySelector("#chaptername").textContent = chapterName;
+      getQuerySelector("#speed").textContent = `${Math.floor(state.speed / 1024) } kB/s`;
+      getQuerySelector("#timeLeft").textContent = `${downloadTimeRemaining(minutes,'0',2)+'m:'+downloadTimeRemaining(seconds,'0',2)}s remaining`;
+      getQuerySelector("#transferred").textContent = `${formatBytes(state.size.transferred)} transferred`;
+      getQuerySelector("#dynamic").style.width = `${Math.floor(state.percent * 100)}%`;
+      getQuerySelector("#dynamic").setAttribute("aria-valuenow", Math.floor(state.percent * 100));
+      getQuerySelector("#dynamic").textContent = `${Math.floor(state.percent * 100)}%`;
+    })
+    .on('error', err => {
+      console.log(err);
+      alert("Connection error. Please check your internet connectivity." + err);
+      app.relaunch();
+      app.exit(0);
+    })
+    .on('end', () => {
+      fs.appendFile(path.resolve(`${dwnpath}${path.sep}chapters.txt`), chapterName + '\n', (err) => {
+        if (err) console.log(err);
+      });
+      nextVideo();
+    })
+    .pipe(fs.createWriteStream(path.resolve(`${dwnpath}${path.sep}${chapterName}.mp4`)));
+}
+
+module.exports = {
+  replaceAll: replaceAll,
+  getQuerySelector: getQuerySelector,
+  getDownloadedVideos: getDownloadedVideos,
+  isComplete: isComplete,
+  findVidExist: findVidExist,
+  download: downloadOne
+};
